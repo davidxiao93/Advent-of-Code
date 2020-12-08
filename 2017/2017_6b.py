3fff72b23e2112cb9842edd8d39ad03270f41857
David Xiao <davidxiao93@gmail.com>
2020-12-08 21:32:10 +0000

    Add solutions to day 5a - 9b 2017

---
+++ 2017/2017_6b.py
@@ -0,0 +1,37 @@
+input = """5	1	10	0	1	7	13	14	3	12	8	10	7	12	0	6"""
+
+# input = """0 2 7 0"""
+
+banks = (int(x) for x in input.split())
+
+def redistribute(b):
+    bs = list(b)
+    largest = 0
+    largest_index = 0
+    for i, m in enumerate(bs):
+        if m > largest:
+            largest = m
+            largest_index = i
+
+    i = (largest_index + 1)%len(bs)
+    bs[largest_index] = 0
+    for _ in range(largest):
+        bs[i] += 1
+        i = (i + 1) % len(bs)
+    return tuple(bs)
+
+
+seen_states = {
+    banks: 0
+}
+current_state = banks
+counter = 0
+while True:
+    counter += 1
+    new_banks = redistribute(banks)
+    banks = new_banks
+    if new_banks in seen_states:
+        print(counter - seen_states[new_banks])
+        break
+    seen_states[new_banks] = counter
+
