86b64d810ad361531ff499506c75c289a51094c7
David Xiao <davidxiao93@gmail.com>
2020-12-08 17:24:22 +0000

    Add solutions to day 24a - 24b 2016 and 3a - 4b 2017 and 7a - 7b 2020

---
+++ 2017/2017_3a.py
@@ -0,0 +1,28 @@
+input = 325489
+
+
+
+if input == 1:
+    print(0)
+    exit()
+
+
+n = 0
+while True:
+    if (2*n+1)*(2*n+1) + 1 <= input <= (2*n+3)*(2*n+3):
+        break
+    n += 1
+distance_along_axis = n + 1
+
+# bottom right corner = (2*n+3)*(2*n+3) ?
+step = 2 * distance_along_axis
+
+axis = [(2*n+1) * (2*n+1) + step*x for x in [0.5, 1.5, 2.5, 3.5]]
+
+distance_to_axis = int(min([abs(input - x) for x in axis]))
+# print(axis)
+# print(distance_to_axis)
+# print(distance_along_axis)
+
+print(distance_to_axis + distance_along_axis)
+
---
+++ 2017/2017_3b.py
@@ -0,0 +1,51 @@
+input = 325489
+
+from collections import namedtuple
+
+Point = namedtuple("Point", ["x", "y"])
+
+
+def get_distance_squared(p: Point):
+    return p.x * p.x + p.y * p.y
+
+def add_point(p: Point, q:Point):
+    return Point(x=p.x+q.x, y=p.y+q.y)
+
+
+current_pos = Point(x=1,y=1)
+grid = {
+    Point(x=0, y=0): 1,
+    Point(x=1, y=0): 1,
+    Point(x=1, y=1): 2
+}
+
+up = Point(x=0, y=1)
+down = Point(x=0, y=-1)
+left = Point(x=-1, y=0)
+right = Point(x=1, y=0)
+
+up_left = add_point(up, left)
+up_right = add_point(up, right)
+down_left = add_point(down, left)
+down_right = add_point(down, right)
+
+traverse_directions = {up, down, left, right}
+sum_directions = {up_left, up, up_right, right, down_right, down, down_left, left}
+
+
+
+while grid[current_pos] < input:
+    # pick next point to go to
+    possible_next_pos = [add_point(current_pos, t) for t in traverse_directions
+                         if add_point(current_pos, t) not in grid]
+    possible_next_pos.sort(key=lambda np: get_distance_squared(np))
+    next_pos = possible_next_pos[0]
+    s = sum([grid[add_point(next_pos, sd)] for sd in sum_directions
+             if add_point(next_pos, sd) in grid])
+    grid[next_pos] = s
+    current_pos = next_pos
+
+
+print(grid[current_pos])
+
+
