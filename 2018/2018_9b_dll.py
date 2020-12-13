35ae7d8e6b5e08afcc186146269fb5dd88e49292
David Xiao <davidxiao93@gmail.com>
2020-12-13 10:15:58 +0000

    Add solutions to day 9b 2018

---
+++ 2018/2018_9b_dll.py
@@ -0,0 +1,58 @@
+input = """477 players; last marble is worth 70851 points"""
+
+words = input.split()
+highest_marble = int(words[-2]) * 100 # Part 2
+num_players = int(words[0])
+
+class Node:
+    def __init__(self, data, next=None, prev=None):
+        self.data = data
+        self.next = next if next is not None else self
+        self.prev = prev if prev is not None else self
+
+
+class Marbles:
+    def __init__(self):
+        self.current_node = Node(0)
+
+    def insert(self, marble):
+        score = 0
+        if marble % 23 == 0:
+            marble_to_remove = self.current_node.prev.prev.prev.prev.prev.prev.prev
+            left_node = marble_to_remove.prev
+            right_node = marble_to_remove.next
+            left_node.next = right_node
+            right_node.prev = left_node
+            self.current_node = right_node
+            score = marble_to_remove.data + marble
+        else:
+            left_node = self.current_node.next
+            right_node = self.current_node.next.next
+            new_node = Node(marble, right_node, left_node)
+            left_node.next = new_node
+            right_node.prev = new_node
+            self.current_node = new_node
+        return score
+
+    def pretty_print(self):
+        ms = [self.current_node.data]
+        next_node = self.current_node.next
+        while next_node.data not in ms:
+            ms.append(next_node.data)
+            next_node = next_node.next
+        print(" ".join([str(m) for m in ms]))
+
+
+marbles = Marbles()
+elf_scores = {}
+for marble in range(1, highest_marble + 1):
+    score = marbles.insert(marble)
+    if score != 0:
+        elf = ((marble - 1) % num_players) + 1
+        if elf not in elf_scores:
+            elf_scores[elf] = 0
+        elf_scores[elf] += score
+
+print(max(elf_scores.values()))
+
+
