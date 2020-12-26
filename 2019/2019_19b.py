f28ae8fb6796336c7a22c47329761d25058175af
David Xiao <davidxiao93@gmail.com>
2020-12-26 21:35:57 +0000

    Add solutions to day 19a - 19b 2019

---
+++ 2019/2019_19b.py
@@ -0,0 +1,220 @@
+from typing import List, Tuple, Set, Dict
+from collections import namedtuple
+
+input = """109,424,203,1,21101,0,11,0,1105,1,282,21101,18,0,0,1105,1,259,2101,0,1,221,203,1,21102,1,31,0,1105,1,282,21101,0,38,0,1106,0,259,21002,23,1,2,22101,0,1,3,21101,0,1,1,21102,1,57,0,1106,0,303,2102,1,1,222,21001,221,0,3,20102,1,221,2,21101,0,259,1,21101,80,0,0,1106,0,225,21101,0,23,2,21102,91,1,0,1106,0,303,1201,1,0,223,20101,0,222,4,21101,0,259,3,21102,1,225,2,21102,1,225,1,21102,1,118,0,1105,1,225,20102,1,222,3,21101,0,87,2,21101,133,0,0,1106,0,303,21202,1,-1,1,22001,223,1,1,21101,0,148,0,1105,1,259,2101,0,1,223,20102,1,221,4,21002,222,1,3,21101,0,9,2,1001,132,-2,224,1002,224,2,224,1001,224,3,224,1002,132,-1,132,1,224,132,224,21001,224,1,1,21102,1,195,0,106,0,109,20207,1,223,2,21001,23,0,1,21102,1,-1,3,21101,0,214,0,1106,0,303,22101,1,1,1,204,1,99,0,0,0,0,109,5,2102,1,-4,249,21201,-3,0,1,22101,0,-2,2,21202,-1,1,3,21102,250,1,0,1106,0,225,21202,1,1,-4,109,-5,2106,0,0,109,3,22107,0,-2,-1,21202,-1,2,-1,21201,-1,-1,-1,22202,-1,-2,-2,109,-3,2105,1,0,109,3,21207,-2,0,-1,1206,-1,294,104,0,99,21202,-2,1,-2,109,-3,2105,1,0,109,5,22207,-3,-4,-1,1206,-1,346,22201,-4,-3,-4,21202,-3,-1,-1,22201,-4,-1,2,21202,2,-1,-1,22201,-4,-1,1,22102,1,-2,3,21102,1,343,0,1106,0,303,1106,0,415,22207,-2,-3,-1,1206,-1,387,22201,-3,-2,-3,21202,-2,-1,-1,22201,-3,-1,3,21202,3,-1,-1,22201,-3,-1,2,21201,-4,0,1,21102,384,1,0,1105,1,303,1106,0,415,21202,-4,-1,-4,22201,-4,-3,-4,22202,-3,-2,-2,22202,-2,-4,-4,22202,-3,-2,-3,21202,-4,-1,-2,22201,-3,-2,1,21202,1,1,-4,109,-5,2106,0,0"""
+
+int_codes = [int(x) for x in input.split(",")]
+
+Param = namedtuple("Param", ["mode", "value"])
+
+opcodes = {
+    1: (3, "add"),
+    2: (3, "multiply"),
+    3: (1, "read-input"),
+    4: (1, "write-output"),
+    5: (2, "jump-if-true"),
+    6: (2, "jump-if-false"),
+    7: (3, "less-than"),
+    8: (3, "equals"),
+    9: (1, "adjust-rel-base"),
+    99: (0, "halt")
+}
+
+class Computer:
+    def __init__(self, int_codes: List[int]):
+        self.int_codes = {
+            i: v
+            for i, v in enumerate(int_codes)
+        }
+        self.current_instruction = 0
+        self.inputs_queue = []
+        self.outputs_queue = []
+        self.halted = False
+        self.waiting_for_input = False
+        self.relative_base = 0
+
+    def _parse_next_instruction(self) -> Tuple[str, List[Param]]:
+        opcode = self.int_codes[self.current_instruction] % 100
+        if opcode not in opcodes:
+            print("unknown opcode")
+            exit(1)
+        params = []
+        parameter_mode = self.int_codes[self.current_instruction] // 100
+        param_values = [
+            self.int_codes[x]
+            for x in range(
+                self.current_instruction + 1,
+                self.current_instruction + 1 + opcodes[opcode][0]
+            )
+        ]
+        for param_value in param_values:
+            param_mode = parameter_mode % 10
+            params.append(Param(param_mode, param_value))
+            parameter_mode = parameter_mode // 10
+        return opcodes[opcode][1], params
+
+    def _read_param(self, param: Param):
+        if param.mode == 0:
+            key = param.value
+        elif param.mode == 1:
+            return param.value
+        elif param.mode == 2:
+            key = param.value + self.relative_base
+        else:
+            print("Unknown param mode for reading")
+            exit(1)
+            return # shut up the linter
+        if key < 0:
+            print("Cannot read from negative address")
+            exit(1)
+        if key not in self.int_codes:
+            self.int_codes[key] = 0
+        return self.int_codes[key]
+
+    def _write_param(self, param: Param, value: int):
+        if param.mode == 0:
+            key = param.value
+        elif param.mode == 1:
+            print("Cannot write when param mode is 1")
+            exit(1)
+            return # shut up the linter
+        elif param.mode == 2:
+            key = param.value + self.relative_base
+        else:
+            print("Unknwon param mode for writing")
+            exit(1)
+            return # shut up the linter
+        if key < 0:
+            print("Cannot write to negative address")
+            exit(1)
+        self.int_codes[key] = value
+
+    def put_input(self, i):
+        self.inputs_queue.append(i)
+        self.waiting_for_input = False
+
+    def has_output(self):
+        return len(self.outputs_queue) > 0
+
+    def pop_next_output(self):
+        if len(self.outputs_queue) > 0:
+            return self.outputs_queue.pop(0)
+        return None
+
+    def run_step(self):
+        if self.halted:
+            return
+        opcode, params = self._parse_next_instruction()
+        jumped = False
+        if opcode == "add":
+            self._write_param(params[2], self._read_param(params[0]) + self._read_param(params[1]))
+        elif opcode == "multiply":
+            self._write_param(params[2], self._read_param(params[0]) * self._read_param(params[1]))
+        elif opcode == "read-input":
+            if len(self.inputs_queue) == 0:
+                # Can't do anything now
+                self.waiting_for_input = True
+                return
+            self._write_param(params[0], self.inputs_queue.pop(0))
+            self.waiting_for_input = False
+        elif opcode == "write-output":
+            self.outputs_queue.append(self._read_param(params[0]))
+        elif opcode == "jump-if-true":
+            if self._read_param(params[0]) != 0:
+                self.current_instruction = self._read_param(params[1])
+                jumped = True
+        elif opcode == "jump-if-false":
+            if self._read_param(params[0]) == 0:
+                self.current_instruction = self._read_param(params[1])
+                jumped = True
+        elif opcode == "less-than":
+            self._write_param(params[2], 1 if self._read_param(params[0]) < self._read_param(params[1]) else 0)
+        elif opcode == "equals":
+            self._write_param(params[2], 1 if self._read_param(params[0]) == self._read_param(params[1]) else 0)
+        elif opcode == "halt":
+            self.halted = True
+        elif opcode == "adjust-rel-base":
+            self.relative_base += self._read_param(params[0])
+        else:
+            print("unknown opcode")
+            exit(1)
+        if not jumped:
+            self.current_instruction += 1 + len(params)
+
+
+
+Point = namedtuple("Point", ["x", "y"])
+up = Point(x=0, y=-1)
+down = Point(x=0, y=1)
+left = Point(x=-1, y=0)
+right = Point(x=1, y=0)
+directions = [up, right, down, left]
+
+def add_point(p: Point, q: Point):
+    return Point(p.x + q.x, p.y + q.y)
+
+
+beam: Dict[Point, bool] = {
+    Point(0,0): True
+}
+
+def is_in_beam(p: Point) -> bool:
+    if p not in beam:
+        c = Computer(int_codes)
+        c.put_input(p.x)
+        c.put_input(p.y)
+        while not c.halted and not c.has_output() and not c.waiting_for_input:
+            c.run_step()
+        if not c.has_output():
+            print("oh dear")
+            exit(1)
+        beam[p] = c.pop_next_output() == 1
+    return beam[p]
+
+
+
+"""
+instead of covering the entire area, we know that the beam is a single entity
+and therefore has a left and right bound
+so we can follow each of these bounds rather than covering the entire area
+"""
+
+left_bound = Point(0,0)
+right_bound = Point(0,0)
+
+def find_next_left_bound(old_left_bound: Point) -> Point:
+    y = old_left_bound.y + 1
+    x = old_left_bound.x
+    while True:
+        if is_in_beam(Point(x, y)):
+            return Point(x, y)
+        x += 1
+
+def find_next_right_bound(old_right_bound: Point) -> Point:
+    y = old_right_bound.y + 1
+    x = old_right_bound.x
+    while not is_in_beam(Point(x, y)):
+        x += 1
+    while True:
+        if not is_in_beam(Point(x, y)):
+            return Point(x-1, y)
+        x += 1
+
+"""
+The target square is 100x100
+This means that from the right bound, there must be 100 units leftwards in beam, and 100 units downwards
+i.e. the left bound 100 rows down must be more than 99 units leftwards of the right bound
+"""
+target_square = 100
+
+# give the left bound a head start
+for i in range(target_square - 1):
+    left_bound = find_next_left_bound(left_bound)
+
+while right_bound.x - left_bound.x < target_square - 1:
+    left_bound = find_next_left_bound(left_bound)
+    right_bound = find_next_right_bound(right_bound)
+
+print(left_bound.x * 10000 + right_bound.y)
+
+
