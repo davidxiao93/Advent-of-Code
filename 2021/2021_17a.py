10b5d8beab1bd88332a4eb716c5938fd878d6e15
David Xiao <davidxiao93@gmail.com>
2021-12-17 11:20:16 +0000

    2021 day 17 solution

---
+++ 2021/2021_17a.py
@@ -0,0 +1,24 @@
+import re
+
+input = """target area: x=57..116, y=-198..-148"""
+
+min_x, max_x, min_y, max_y = [int(a) for a in
+    re.search(r"^target area: x=([-]?\d+)\.\.([-]?\d+), y=([-]?\d+)\.\.([-]?\d+)", input).groups()
+]
+
+"""
+To get maximum height, we need to max out our initial y speed whilst still hitting the target box
+Our initial position is 0,0
+Say our initial velocity is vx, vy
+Observation: after 2*vy + 1 steps, the y-axis position of the probe will always be 0
+Therefore, at 2*vy + 2 steps, the y coordinate of the probe is -1*vy - 1
+Thus the maximum vy that still lands inside the target box is such that -1*vy - 1 = bottom of the target
+"""
+
+vy = -1*min_y - 1
+
+"""
+maximum height achieved when given initial y velocity of vy is vy(vy+1)/2
+"""
+
+print(int((vy*(vy+1)) / 2))
---
+++ 2021/2021_17b.py
@@ -0,0 +1,49 @@
+import re
+
+input = """target area: x=57..116, y=-198..-148"""
+# input = """target area: x=20..30, y=-10..-5"""
+min_x, max_x, min_y, max_y = [int(a) for a in
+    re.search(r"^target area: x=([-]?\d+)\.\.([-]?\d+), y=([-]?\d+)\.\.([-]?\d+)", input).groups()
+]
+
+x_steps_taken = dict()
+y_steps_taken = dict()
+
+for vy in range(min_y - 1, -1*min_y + 1):
+    steps = 0
+    current_y = 0
+    vel_y = vy
+    while current_y > (min_y - 1):
+        steps += 1
+        current_y += vel_y
+        if min_y <= current_y <= max_y:
+            if steps not in y_steps_taken:
+                y_steps_taken[steps] = []
+            y_steps_taken[steps].append(vy)
+        vel_y -= 1
+
+for vx in range(1, max_x + 1):
+    steps = 0
+    current_x = 0
+    vel_x = vx
+    while current_x <= max_x and steps < max(y_steps_taken.keys()):
+        steps += 1
+        current_x += vel_x
+        if steps in y_steps_taken and min_x <= current_x <= max_x:
+            if steps not in x_steps_taken:
+                x_steps_taken[steps] = []
+            x_steps_taken[steps].append(vx)
+
+        if vel_x > 0:
+            vel_x -= 1
+
+
+count = set()
+for k, v in x_steps_taken.items():
+    # Have to use set as there can be (x,y) solutions that reach
+    # the target box in multiple number of steps
+    count = count.union({(x, y) for x in v for y in y_steps_taken[k]})
+
+print(len(count))
+
+
