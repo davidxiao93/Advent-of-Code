552d78d83cc40f280d9e66e50adde5de300ff43b
David Xiao <davidxiao93@gmail.com>
2022-12-07 19:33:17 +0000

    2022 day 7

---
+++ 2022/07a.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+from typing import List, Dict, Tuple
+
+import file_loader
+
+input_string = file_loader.get_input()
+
+
+Output = List[str]
+Command = List[str]
+commands: List[Tuple[Command, Output]] = []
+current_command: Tuple[Command, Output] = ([], [])
+for line in input_string.splitlines():
+    if line.startswith("$"):
+        if current_command != ([], []):
+            commands.append(current_command)
+        current_command = (line.split()[1:], [])
+    else:
+        current_command = (current_command[0], current_command[1] + [line])
+commands.append(current_command)
+
+
+class Directory:
+    def __init__(self):
+        self.parent: Directory = None
+        self.files: Dict[str, int] = {}
+        self.directories: Dict[str, Directory] = {}
+        self.total_size = -1
+
+    def size(self) -> int:
+        if self.total_size != -1:
+            return self.total_size
+        return sum(self.files.values()) + sum([d.size() for d in self.directories.values()])
+
+
+root = Directory()
+
+pwd = root
+output: List[str] = []
+for command in commands:
+    c = command[0]
+    if c[0] == "ls":
+        for o in command[1]:
+            if o.startswith("dir"):
+                dirname = o.split()[-1]
+                if dirname not in pwd.directories:
+                    pwd.directories[dirname] = Directory()
+                    pwd.directories[dirname].parent = pwd
+            else:
+                size = int(o.split()[0])
+                filename = o.split()[-1]
+                if filename not in pwd.files:
+                    pwd.files[filename] = size
+    elif c[0] == "cd":
+        if c[1] == "..":
+            pwd = pwd.parent
+        elif c[1] == "/":
+            pwd = root
+        else:
+            dirname = c[1]
+            pwd = pwd.directories[dirname]
+
+size = 0
+dirs_to_check = [root]
+while len(dirs_to_check) > 0:
+    d = dirs_to_check.pop(0)
+    if d.size() < 100_000:
+        size += d.size()
+    dirs_to_check += d.directories.values()
+
+print(size)
